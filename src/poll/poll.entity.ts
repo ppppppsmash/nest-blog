adea8f1ad6049d3647161f998ce7de3b87057d8e
ppppppsmash <pppppp.work@gmail.com>
2023-06-08 23:56:17 +0900

    add: typeormとentity

---
+++ src/poll/poll.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  confirmed: boolean;
+}
