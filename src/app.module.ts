2f8438608e99771ce949d6770331a4d81e28a69f
ppppppsmash <pppppp.work@gmail.com>
2023-06-08 23:59:10 +0900

    add: userモジュール

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PollModule } from './poll/poll.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PollModule],
+  imports: [PollModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
