aee2facad7dbbcf93a8b0e0decc72eb37974b2f3
ppppppsmash <pppppp.work@gmail.com>
2023-07-22 23:14:30 +0900

    add: postインターフェース&postsサービスメソッド

---
+++ src/posts/post.interface.ts
@@ -0,0 +1,7 @@
+export interface PostType {
+  id: string;
+  title: string;
+  content: string;
+  authoer: string;
+  createAt: string;
+}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
-import { PostsService } from './posts/posts.service';
 import { PostsService } from './posts.service';
 
 @Module({
